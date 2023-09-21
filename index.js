d8a14828c88a10bbb55f981204d003c598b070d9
lexanders7 <ga.nyaga7@gmail.com>
2023-09-21 13:45:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const main = document.querySelector('main#main');
+
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+
+const yourName = 'Alexander'; 
+newHeader.textContent = `${yourName} is the champion`;
